25f9d48b6dcd25243e7d53daa74f77758a65ae92
sradhanjan <56840012+sradhanjan@users.noreply.github.com>
2020-07-03 18:41:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function menuToggle(){
+		var nav = document.getElementById("nav");
+		nav.classList.toggle('active')
+        var toggle = document.getElementById("toggle");
+        toggle.classList.toggle('active')
+	}
